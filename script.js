6ea08b6f816db94cd241dc545ec078d428cab46d
KazeStore <galangali300@gmail.com>
2025-12-24 10:34:52 +0900

    Wrap event listener in DOMContentLoaded

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // JavaScript for future functionalities (e.g., adding products to cart)
 
-document.querySelectorAll('.product button').forEach(button => {
-  button.addEventListener('click', () => {
-    alert('Item added to cart!');
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.product button').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Item added to cart!');
+    });
   });
 });
