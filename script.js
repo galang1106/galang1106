3bbb180a6d558d8eb9d7ed9a21f2bbc1aa3e4055
KazeStore <galangali300@gmail.com>
2026-04-25 22:24:43 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for future functionalities (e.g., adding products to cart)
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.product button').forEach(button => {
-    button.addEventListener('click', () => {
-      alert('Item added to cart!');
-    });
-  });
-});
